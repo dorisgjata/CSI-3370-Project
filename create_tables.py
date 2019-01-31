361015c10bf97e2c4c632e4406e26066a839f27c
dorisgjata <dorisgjata@oakland.edu>
2019-01-31 15:50:19 -0500

    Created separate file to do the creation of tables

---
+++ create_tables.py
@@ -0,0 +1,30 @@
+import psycopg2
+
+def createtables():
+    conn = None
+    #reading the table.sql file to execute commands
+    fd = open('tables.sql', 'r')
+    sqlFile=fd.read()
+    fd.close()
+    sqlCommands = sqlFile.split(';')
+
+    try:
+        print('Connecting to the PostgreSQL database')
+        conn = psycopg2.connect(host="localhost", database="dinning", user="postgres", password="postgres")
+        cur=conn.cursor()
+        for command in sqlCommands:
+            #the file contains comments that are parsed into emty strngs
+            if command !="":
+                cur.execute(command)
+        cur.close()
+        conn.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+if __name__ == '__main__':
+    createtables()
