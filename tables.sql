86db6c889af3b0d9eb0d4acbcda8b9c18625ece1
dorisgjata <dorisgjata@oakland.edu>
2019-01-31 13:54:25 -0500

    Added more tables and fixed relations

--- tables.sql
+++ tables.sql
@@ -1,59 +1,95 @@
 DROP TABLE IF EXISTS campusLocations CASCADE;
-DROP TABLE IF EXISTS menu CASCADE;
-DROP TABLE IF EXISTS periods CASCADE;
-DROP TABLE IF EXISTS categories CASCADE;
-DROP TABLE IF EXISTS items CASCADE;
 DROP TABLE IF EXISTS filters CASCADE;
+DROP TABLE IF EXISTS items CASCADE;
+DROP TABLE IF EXISTS menu CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS periods CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS storedPreferences CASCADE;
+DROP TABLE IF EXISTS meal CASCADE;
+DROP TABLE IF EXISTS favourites CASCADE;
 
+--Hillcrest or Vanderberg ---
 CREATE TABLE campusLocations(
     locationId INT PRIMARY KEY,
     locationName TEXT,
     startHours TIME,
     endHours TIME
     );
-CREATE TABLE menu(
-    menuId INT PRIMARY KEY,
-    menuDate DATE,
-    menuName TEXT,
-    fromDate DATE,
-    toDate DATE,
-    locationID INT REFERENCES campusLocations(locationId), 
-    itemID INT REFERENCES items (itemId) 
+
+CREATE TABLE filters(
+    filterId INT PRIMARY KEY,
+    filterName TEXT
 );
-CREATE TABLE periods(
-    periodId INT PRIMARY KEY,
-    periodName TEXT,
-    periodCategory INT REFERENCES categories(categoryId)
-   );
-CREATE TABLE categories(
-    categoryId INT PRIMARY KEY,
-    categoryName TEXT,
-    categoryItems INT REFERENCES items(itemId)
-   );
+--Food items--   
 CREATE TABLE items(
     itemId INT PRIMARY KEY,
     itemName TEXT,
     itemPortion TEXT,
     itemIngridents TEXT,
     itemNutrients TEXT,
-    itemFilters INT REFERENCES filters(filterID)
+    itemFilters INT REFERENCES filters(filterId)
+    ON UPDATE CASCADE ON DELETE CASCADE
 );
-CREATE TABLE filters(
-    filterId INT PRIMARY KEY,
-    filterName TEXT
+--Menu for a specific time period---   
+CREATE TABLE menu(
+    menuId INT PRIMARY KEY,
+    menuDate DATE,
+    menuName TEXT,
+    fromDate DATE,
+    toDate DATE,
+    locationID INT REFERENCES campusLocations(locationId)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    foodItem INT REFERENCES items(itemId)
+    ON UPDATE CASCADE ON DELETE CASCADE
 );
-
+CREATE TABLE categories(
+    categoryId INT PRIMARY KEY,
+    categoryName TEXT,
+    categoryItems INT REFERENCES items(itemId)
+    ON UPDATE CASCADE ON DELETE CASCADE
+   );
+--Brakfast, lunch, dinner, snack, late night--
+CREATE TABLE periods(
+    periodId INT PRIMARY KEY,
+    periodName TEXT,
+    periodCategory INT REFERENCES categories(categoryId)
+    ON UPDATE CASCADE ON DELETE CASCADE
+   );
+--user account--
 CREATE TABLE users(
     userId INT PRIMARY KEY,
     userName TEXT,
     userPassword TEXT,
-    userPreferences TEXT
+    userPreferences TEXT,
+    isAdmin BOOLEAN
 );
-DROP TABLE IF EXISTS  preferences CASCADE;
-CREATE TABLE preferences(
+--users preferences--
+CREATE TABLE storedPreferences(
     preferenceId INT PRIMARY KEY,
     preferenceCalories TEXT ,
     preferenceNutrients TEXT,
-    preferenceFilters INT REFERENCES filters(filterID)
+    preferenceFilters INT REFERENCES filters(filterId)
+    ON UPDATE CASCADE ON DELETE CASCADE
+    );
+--daily meal with max 3 items--
+CREATE TABLE meal(
+    mealId INT PRIMARY KEY,
+    foodItem1 INT REFERENCES items (itemId)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    foodItem2 INT REFERENCES items (itemId)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    foodItem3 INT REFERENCES items (itemId)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    mealPeriod INT REFERENCES periods(periodId)
+    ON UPDATE CASCADE ON DELETE CASCADE
+);
+--users saved favourites--
+CREATE TABLE favourites(
+    favouriteId INT PRIMARY KEY,
+    favouriteRecommendation INT REFERENCES meal(mealId) 
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    favouriteMeal INT REFERENCES meal(mealId) 
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    favouriteCalories TEXT
 );
