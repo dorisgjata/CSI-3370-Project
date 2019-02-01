13d9cb2ec79856c02a12acd10bc18c1ee233d176
dorisgjata <dorisgjata@oakland.edu>
2019-01-31 20:08:25 -0500

    Fetching locations from database and displaying them on the web app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+import psycopg2
 
+from database import conn_close,conn_open
 app = Flask(__name__)
+conn = psycopg2.connect(host="localhost", database="dinning", user="postgres", password="postgres")
 
 @app.route('/')
 def index():
     return 'Hello World'
 
+@app.route('/locations')
+def locations():
+    sql = 'SELECT locationname FROM campuslocations'
+    cur=conn.cursor()
+
+    cur.execute(sql)
+    locations=cur.fetchone()[0]
+    loc=[]
+    for location in locations:
+        loc.append(location)
+
+    conn.commit()
+
+    cur.close()
+    return render_template('main.html', location=locations)
 
 if __name__ == '__main__':
     app.run(debug=True)
