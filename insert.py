83a1da60575202e73a4c63a167276be10b3e92dc
dorisgjata <dorisgjata@oakland.edu>
2019-03-04 09:22:01 -0500

    Inserted filters

--- insert.py
+++ insert.py
@@ -3,13 +3,15 @@ import psycopg2
 def insert(param):
     conn = None
     sql = "INSERT INTO campusLocations(locationId, locationName) VALUES (%s, %s)"
+    filtersql = "INSERT INTO filters(filterId, filterName) VALUES (%s, %s)"
+    usersql = "INSERT INTO users(userId, userName, userPassword, userPreferences, isAdmin ) VALUES (%s, %s)"
 
     try:
         print('Connecting to the PostgreSQL database')
         conn = psycopg2.connect(host="localhost", database="dinning", user="postgres", password="postgres")
         cur=conn.cursor()
 
-        cur.executemany(sql,param)
+        cur.executemany(filtersql,param)
         conn.commit()
         cur.close()
 
@@ -22,3 +24,5 @@ def insert(param):
 
 if __name__ == '__main__':
   #  insert([(1,'Hillcrest'),(2,'Vanderberg')])
+  #insert([(1,'Vegetarian'),(2,'Vegan'),(3,'Balanced'),(4,'Milk'),(5,'Tomato'),(6,'Gluten'),(7,'Wheat'),(8,'Nuts')])
+    insert([(1,'dorisgjata', 'admin', 'none', True ),(1,'olivia', 'olivia', 'none', False )])
