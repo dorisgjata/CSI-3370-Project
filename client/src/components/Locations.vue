94cc799a85db55c50e2cee63a4ec3e67e3d7ef3f
dorisgjata <dorisgjata@oakland.edu>
2019-03-04 09:27:54 -0500

    Added vue for frontend

---
+++ client/src/components/Locations.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="conatiner">
+    <h1>{{ msg }}</h1>
+    
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Locatons',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+  methods: {
+    getLocations() {
+      const path = 'http://localhost:5000/locations';
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getLocations();
+  },
+};
+</script>
---
+++ client/src/components/Menu.vue
@@ -0,0 +1,84 @@
+<template>
+    <b-table :data="msg" width="100%" >
+        <template slot-scope="props">
+                <b-table-column field="name" label="Period Name" width="50">
+                    {{ props.row.name }}
+                </b-table-column>
+                <b-table-column field="categories"  label="Category Name" width="100" >
+                    <b-field > 
+                        <b-select v-model="filters" >
+                            <option
+                        
+                                v-for="category in props.row.categories"
+                                :value="category.id"
+                                :key="category.id">
+                                {{ category.name }}
+                            </option>
+                        </b-select>
+                    </b-field>                    
+               
+           <!--      <b-table-column label="Items">
+                     <b-select v-model="items">
+                       <option
+                        :key="item.itemid"
+                        :value="item.itemid"
+                         v-for="item  in category.items">
+                         {{ item.name }}</option>
+                       </b-select>    
+                </b-table-column> -->
+                                </b-table-column>
+
+
+            </template>
+    </b-table>
+
+</template>
+
+
+<script>
+import axios from 'axios';
+const data = require('../assets/menu.json')
+
+export default {
+  name: 'Menu',
+  data() {
+    return {
+                        columns: [                    
+                    {
+                        field: 'name',
+                        label: 'Period Name ',
+                    },
+                    {
+                        field: 'periods',
+                        label: 'Periods',
+                    },
+                    {
+                        field: 'periods.categories.name',
+                        label: 'Category',
+                    },
+                    ],
+     // msg: [],
+     msg: data.menu.periods,
+      filters: [],
+      items: []
+    };
+  },
+ /*  methods: {
+    getLocations() {
+      const path = 'http://localhost:5000/menu';
+      axios.get(path)
+        .then((res) => {
+          console.log(res);  
+          this.msg = res.data.menu.periods;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getLocations();
+  }, */
+};
+</script>
