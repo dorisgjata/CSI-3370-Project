fd2819eca509b3018c38318e3a3db35b93ec3d1c
dorisgjata <dorisgjata@oakland.edu>
2019-04-03 16:29:51 -0400

    Added what's on menu component to menu path

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,11 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Locations from '@/components/Locations';
+import WhatsOnMenu from '@/components/WhatsOnMenu';
 import Menu from '@/components/Menu';
-import Filters from '@/components/Filters';
 import Account from '@/components/Account';
 import Steps from '@/components/Steps';
-import Items from '@/components/Items';
 
 
 
@@ -13,35 +11,25 @@ Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/locations',
-      name: 'Locations',
-      component: Locations,
-    },
       {
         path: '/account/:email',
         name: 'account',
         component: Account,
       },
     {
-      path: '/filters',
-      name: 'Filters',
-      component: Filters,
+      path: '/parsedmenu',
+      name: 'Menu',
+      component: Menu,
     },
     {
       path: '/menu',
-      name: 'Menu',
-      component: Menu,
+      name: 'WhatsOnMenu',
+      component: WhatsOnMenu,
     },
     {
       path: '/steps',
       name: 'steps',
       component: Steps,
-    },
-    {
-      path: '/items',
-      name: 'items',
-      component: Items,
     },
   ],
   mode: 'history',
